6faf69c744666adf8a18f9929a949765de8a0fa5
WGIceKing <44535049+WGIceKing@users.noreply.github.com>
2022-12-06 20:44:45 +0100

    [Data preparation] Export formated przekleństwa to file.

---
+++ dict_prep/prepdict.py
@@ -0,0 +1,12 @@
+import re
+
+class toDict():
+    def __init__(this):
+        with open("message.txt", 'r', encoding="utf-8") as file:
+            data = file.read().replace('\n', '')
+            res = re.findall(r'\[\[.*?\]\]', data)
+            with open("refactored.txt", 'w', encoding="utf-8") as save_file:
+                for word in res:
+                    save_file.write(word[2:-2] + '\n')
+
+toDict()
