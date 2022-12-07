eec2123612bf6118a744a0a4ce1ad36903905c5f
Maciej <maciej.majewski13@gmail.com>
2022-12-07 23:27:04 +0100

    [Analyser] Data preprocessing implementation.

--- analyser/repeat_removal.py
+++ analyser/repeat_removal.py
@@ -1,10 +1,28 @@
 import re
+import string
+
+dict = {'1' : 'i',
+        '3': 'e',
+        '4': 'a',
+        '5': 's',
+        '7': 't',
+        '8': 'b',
+        '0': 'o',
+        'v': 'u',
+        'vv': 'w'}
+
 
 class RepeatRemoval():
     def RemoveRepeated(word_list):
 
         return_list = []
         for word in word_list:
+            word = word.lower()
+            
+            for key, initial in dict.items():
+                word = word.replace(key, initial)
+
+            word = word.translate(str.maketrans("","", string.punctuation))
             return_list.append(re.sub(r'(\w)\1+', r'\1', word))
 
         return return_list
