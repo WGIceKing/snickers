6f4956ad300493bf3e20c32870aba28932f46010
Maciej Rogala <maciej@Maciejs-MacBook-Pro.local>
2022-12-07 20:04:35 +0100

    Implement lemmatizer

---
+++ analyser/lemmatizer.py
@@ -0,0 +1,10 @@
+import spacy
+
+# to install "pl_core_news_sm" spacy model use:
+# python -m spacy download pl_core_news_sm
+class Lemmatizer:
+
+    def lemmatize(this, words):
+        nlp = spacy.load("pl_core_news_sm")
+        doc = nlp(" ".join(words))
+        return [token.lemma_ for token in doc]
